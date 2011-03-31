12967497224fc98e6c397a6a4a633f1c3b6ef1da
Lawrence Murray <lawrence.murray@csiro.au>
2011-03-31 05:21:45 +0000

    Added generic function for plotting area between two curves, added plotting script for output of simulate program

---
+++ octave/area_between.m
@@ -0,0 +1,43 @@
+% Copyright (C) 2011
+% Author: Lawrence Murray <lawrence.murray@csiro.au>
+% $Rev$
+% $Date$
+
+% -*- texinfo -*-
+% @deftypefn {Function File} area_between (x, y1, y2, c)
+%
+% Plot area between two curves.
+%
+% @itemize
+% @bullet{ @var{x} X-coordinates.}
+% @bullet{ @var{y1} Y-coordinates of first curve.}
+% @bullet{ @var{y2} Y-coordinates of second curve.}
+% @bullet{ @var{c} Color.}
+% @end itemize
+% @end deftypefn
+%
+function area_between (x, y1, y2, c)
+    % check arguments
+    if (nargin < 3 || nargin > 4)
+        print_usage ();
+    end
+    if (!isvector(x))
+        error ('x must be vector');
+    end
+    if (!isvector(y1))
+        error ('y1 must be vector');
+    end
+    if (!isvector(y2))
+        error ('y2 must be vector');
+    end
+    
+    % plot
+    a = [x x(end:-1:1)];
+    b = [y1 y2(end:-1:1)];
+        
+    if (nargin >= 4)
+        patch(a, b, c, 'linewidth', 0);
+    else
+        patch(a, b, 'linewidth', 0);
+    end
+end
--- octave/twindata.m
+++ octave/twindata.m
@@ -10,49 +10,70 @@
 % program.
 %
 % @itemize
-% @bullet @var{in} Input file. Gives the name of a NetCDF file output by
-% simulate.
+% @bullet{ @var{in} Input file. Gives the name of a NetCDF file output by
+% simulate.}
 %
-% @bullet @var{invars} Cell array of strings naming the variables
-% of this file to disturb.
+% @bullet{ @var{invars} Cell array of strings naming the variables
+% of this file to disturb.}
 %
-% @bullet @var{out} Output file. Name of a NetCDF file to create.
+% @bullet{ @var{out} Output file. Name of a NetCDF file to create.}
 %
-% @bullet @var{outvars} Cell array of strings naming the variables
+% @bullet{ @var{outvars} Cell array of strings naming the variables
 % to create in the output file. Its length must match that of
-% @var{invars}.
+% @var{invars}.}
 %
-% @bullet @var{p} Index along the @t{nr} dimension of the
+% @bullet{ @var{p} Index along the @t{nr} dimension of the
 % input file, indicating the trajectory to disturb to create
-% observations.
+% observations.}
 %
-% @bullet @var{S} List of standard deviations of disturbance
+% @bullet{ @var{S} List of standard deviations of disturbance
 % noise. Each value produces a corresponding record along the @t{ns}
-% dimension of the output file.
+% dimension of the output file.}
 %
-% @bullet @var{log} True for log-normal noise, false for normal noise.
+% @bullet{ @var{logn} True for log-normal noise, false for normal noise.}
 % @end itemize
-%
 % @end deftypefn
-
-function twindata (in, invars, out, outvars, p, S, log)
+%
+function twindata (in, invars, out, outvars, p, S, logn)
     % check arguments
     if (nargin < 6)
         print_usage ();
     end
     if (length (invars) != length (outvars))
-        error ("Lengths of @var{invars} and @var{outvars} must ...
-               match");
+        error ('Lengths of invars and outvars must match');
     end
     if (!isscalar (p))
-        error ("@var{p} must be scalar");
+        error ('p must be scalar');
+    end
+    if (!columns (S) == 1)
+        error ('S must be scalar or column vector');
     end
     
-    % open files
+    % input file
     nci = netcdf(in, 'r');
-    nco = netcdf(out, 'c');
     
+    % output file
+    nco = netcdf(out, 'c');
+    nco('ns') = length(S);
+    nco('nr') = nci('nr')(:);
+    nco('np') = 1;
+    nco{'time'} = ncdouble('nr');
+    %nco{'time'}(:) = nci{'time'}(:);
+
+    % construct data
     for i = 1:length(invars)
-        x = nci{invars{i}}(:,p);
+        x = nci{invars{i}}(:,p)';
+        U = normrnd(0.0, 1.0, length(S), length(x));
+        if logn
+            Y = exp(repmat(log(x), length(S), 1) + repmat(S, 1, length(x)).*U);
+        else
+            Y = repmat(x, length(S), 1) + repmat(S, 1, length(x)).*U;
+        end
+        
+        nco{outvars{i}} = ncdouble('ns', 'nr');
+        nco{outvars{i}}(:,:) = Y;
     end
+    
+    ncclose(nci);
+    ncclose(nco);
 end
