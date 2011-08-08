a9c9db06bd0a486ff33e5e535659fd446bfab5f2
Lawrence Murray <lawrence.murray@csiro.au>
2011-08-08 03:47:54 +0000

    Added rang argument to extract(), fixed support for variables with more than one dimension.

--- octave/extract.m
+++ octave/extract.m
@@ -4,23 +4,28 @@
 % $Date$
 
 % -*- texinfo -*-
-% @deftypefn {Function File} extract (@var{in}, @var{out}, @var{vars})
+% @deftypefn {Function File} extract (@var{in}, @var{out}, @var{vars}, @var{rang})
 %
 % Extract variables from one NetCDF file into another.
 %
 % @itemize
-% @bullet{ @var{in} Input file. Name of a NetCDF file output by simulate.}
+% @bullet{ @var{in} Input file. Name of a NetCDF file.}
 %
 % @bullet{ @var{out} Output file. Name of a NetCDF file to create.}
 %
 % @bullet{ @var{vars} Names of variables as cell array.}
+%
+% @bullet{ @var{rang} Range along time dimension to extract.
 % @end itemize
 % @end deftypefn
 %
-function extract (in, out, vars)
+function extract (in, out, vars, rang)
     % check arguments
     if nargin < 3
         print_usage ();
+    end
+    if nargin < 4
+        rang = [];
     end
 
     % input file
@@ -33,7 +38,15 @@ function extract (in, out, vars)
     dims = ncdim (nci);
     for i = 1:length(dims)
         dim = ncname (dims{i});
-        nco(dim) = length (nci(dim));
+        if strcmp(dim, 'nr')
+            T = length (nci(dim));
+            if isempty (rang)
+                rang = [1:T];
+            end
+            nco(dim) = length (rang);
+        else
+            nco(dim) = length (nci(dim));
+        end
     end
  
     % variables
@@ -51,19 +64,19 @@ function extract (in, out, vars)
                 nco{var}(:) = nci{var}(:);
               case 2
                 nco{var} = ncdouble (ncname (dims{1}), ncname (dims{2}));
-                nco{var}(:) = nci{var}(:,:);
+                nco{var}(:,:) = nci{var}(rang,:);
               case 3
                 nco{var} = ncdouble (ncname (dims{1}), ncname (dims{2}), ...
                     ncname (dims{3}));
-                nco{var}(:) = nci{var}(:,:,:);
+                nco{var}(:,:,:) = nci{var}(rang,:,:);
               case 4
                 nco{var} = ncdouble (ncname (dims{1}), ncname (dims{2}), ...
                     ncname (dims{3}), ncname (dims{4}));
-                nco{var}(:) = nci{var}(:,:,:,:);
+                nco{var}(:,:,:,:) = nci{var}(rang,:,:,:);
               case 5
                 nco{var} = ncdouble (ncname (dims{1}), ncname (dims{2}), ...
                     ncname (dims{3}), ncname (dims{4}), ncname (dims{5}));
-                nco{var}(:) = nci{var}(:,:,:,:,:);
+                nco{var}(:,:,:,:,:) = nci{var}(rang,:,:,:,:);
               otherwise
                 error (sprintf('Variable %s has greater than 5 dimensions', ...
                     var));
