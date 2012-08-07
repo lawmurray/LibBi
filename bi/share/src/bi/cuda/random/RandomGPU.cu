74f12bd031b66d8ff30fe095187ae7d749ad38b1
Lawrence Murray <lawrence.murray@csiro.au>
2012-08-07 04:53:38 +0000

    Corrected seeding of device random number generators when MPI enabled (all processes were being seeded with the same value).

--- bi/share/src/bi/cuda/random/RandomGPU.cu
+++ bi/share/src/bi/cuda/random/RandomGPU.cu
@@ -10,15 +10,26 @@
 #include "RandomKernel.cuh"
 #include "../../cuda/device.hpp"
 
+#ifdef ENABLE_MPI
+#include "boost/mpi/communicator.hpp"
+#endif
+
 using namespace bi;
 
 void RandomGPU::seeds(Random& rng, const unsigned seed) {
+  #ifdef ENABLE_MPI
+  boost::mpi::communicator world;
+  int s = seed + world.rank();
+  #else
+  int s = seed;
+  #endif
+
   dim3 Db, Dg;
   Db.x = deviceIdealThreadsPerBlock();
   Dg.x = deviceIdealThreads()/Db.x;
 
-  kernelSeeds<<<Dg,Db>>>(Random(rng), seed);
-  // ^ copy of *this here seems necessary, otherwise the copy constructor is
+  kernelSeeds<<<Dg,Db>>>(Random(rng), s);
+  // ^ copy of rng here seems necessary, otherwise the copy constructor is
   //   not called, and rng is destroyed on kernel exit!
   CUDA_CHECK;
 }
