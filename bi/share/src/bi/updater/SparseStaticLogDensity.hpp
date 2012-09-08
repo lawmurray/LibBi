36c1e75d12a61c9996031cd68e3b34e2f5d9d139
Lawrence Murray <lawrence.murray@csiro.au>
2012-09-08 04:58:46 +0000

    Disabled SSE for sparse log-density computations due to particle filter runtime error

--- bi/share/src/bi/updater/SparseStaticLogDensity.hpp
+++ bi/share/src/bi/updater/SparseStaticLogDensity.hpp
@@ -98,6 +98,7 @@ template<class B, class S>
 template<class V1>
 void bi::SparseStaticLogDensity<B,S>::logDensities(State<B,ON_HOST>& s,
     const Mask<ON_HOST>& mask, V1 lp) {
+///@todo Fix this -- causes runtime error under particle filter
 //  #ifdef ENABLE_SSE
 //  if (s.size() % BI_SSE_SIZE == 0) {
 //    SparseStaticLogDensitySSE<B,S>::logDensities(s, mask, lp);
