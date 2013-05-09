5adfdcecba813f3ab4e90ab5d3a2a88d14649d3d
Lawrence Murray <lawrence.murray@csiro.au>
2013-05-09 09:01:22 +0000

    Removed OpenMP use in gather and scatter operations on matrices under Intel compiler: segfaulting here again, unable to find any problems in other code with valgrind, so assuming this is a compiler issue for now.

--- bi/share/src/bi/host/primitive/matrix_primitive.hpp
+++ bi/share/src/bi/host/primitive/matrix_primitive.hpp
@@ -66,7 +66,9 @@ struct scatter_matrix_impl<ON_HOST> {
 
 template<class V1, class M1, class M2>
 void bi::gather_rows_impl<bi::ON_HOST>::func(const V1 map, const M1 X, M2 Y) {
+  #ifndef __ICC // Intel compiler producing segfaults under OpenMP here
   #pragma omp parallel for
+  #endif
   for (int j = 0; j < X.size2(); ++j) {
     bi::gather(map, column(X, j), column(Y, j));
   }
@@ -75,7 +77,9 @@ void bi::gather_rows_impl<bi::ON_HOST>::func(const V1 map, const M1 X, M2 Y) {
 template<class V1, class M1, class M2>
 void bi::gather_columns_impl<bi::ON_HOST>::func(const V1 map, const M1 X,
     M2 Y) {
+  #ifndef __ICC // Intel compiler producing segfaults under OpenMP here
   #pragma omp parallel for
+  #endif
   for (int j = 0; j < map.size(); ++j) {
     column(Y, j) = column(X, map(j));
   }
@@ -84,7 +88,9 @@ void bi::gather_columns_impl<bi::ON_HOST>::func(const V1 map, const M1 X,
 template<class V1, class V2, class M1, class M2>
 void bi::gather_matrix_impl<bi::ON_HOST>::func(const V1 map1, const V2 map2,
     const M1 X, M2 Y) {
+  #ifndef __ICC // Intel compiler producing segfaults under OpenMP here
   #pragma omp parallel for
+  #endif
   for (int j = 0; j < map2.size(); ++j) {
     for (int i = 0; i < map1.size(); ++i) {
       Y(i, j) = X(map1(i), map2(j));
@@ -95,7 +101,9 @@ void bi::gather_matrix_impl<bi::ON_HOST>::func(const V1 map1, const V2 map2,
 template<class V1, class M1, class M2>
 void bi::scatter_rows_impl<bi::ON_HOST>::func(const V1 map, const M1 X,
     M2 Y) {
+  #ifndef __ICC // Intel compiler producing segfaults under OpenMP here
   #pragma omp parallel for
+  #endif
   for (int j = 0; j < X.size2(); ++j) {
     bi::scatter(map, column(X, j), column(Y, j));
   }
@@ -104,7 +112,9 @@ void bi::scatter_rows_impl<bi::ON_HOST>::func(const V1 map, const M1 X,
 template<class V1, class M1, class M2>
 void bi::scatter_columns_impl<bi::ON_HOST>::func(const V1 map, const M1 X,
     M2 Y) {
+  #ifndef __ICC // Intel compiler producing segfaults under OpenMP here
   #pragma omp parallel for
+  #endif
   for (int j = 0; j < map.size(); ++j) {
     column(Y, map(j)) = column(X, j);
   }
@@ -113,7 +123,9 @@ void bi::scatter_columns_impl<bi::ON_HOST>::func(const V1 map, const M1 X,
 template<class V1, class V2, class M1, class M2>
 void bi::scatter_matrix_impl<bi::ON_HOST>::func(const V1 map1, const V2 map2,
     const M1 X, M2 Y) {
+  #ifndef __ICC // Intel compiler producing segfaults under OpenMP here
   #pragma omp parallel for
+  #endif
   for (int j = 0; j < map2.size(); ++j) {
     for (int i = 0; i < map1.size(); ++i) {
       Y(map1(i), map2(j)) = X(i, j);
