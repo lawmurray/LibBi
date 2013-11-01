63cd920dab96c0ee0e2433c0e27f1af0de44f00f
Lawrence Murray <lawrence.murray@csiro.au>
2013-11-01 14:24:45 +0800

    Changed default of --adapter-ess-rel to 0.0 for backwards compatibility.

--- lib/Bi/Client/sample.pm
+++ lib/Bi/Client/sample.pm
@@ -132,7 +132,7 @@ Global proposal adaptation.
 When local proposal adaptation is used, the scaling factor of the local
 proposal standard deviation relative to the global sample standard deviation.
 
-=item C<--adapter-ess-rel> (default 0.25)
+=item C<--adapter-ess-rel> (default 0.0)
 
 Threshold for effective sample size (ESS) adaptation trigger. Adaptation will
 only be performed if ESS is above this proportion of C<--nsamples>. To always
@@ -192,7 +192,7 @@ our @CLIENT_OPTIONS = (
     {
       name => 'adapter-ess-rel',
       type => 'float',
-      default => 0.25
+      default => 0.0
     },
 );
 
