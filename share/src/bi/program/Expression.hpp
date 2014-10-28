0070326fbd15af4f2245fddb7ada6b841fb26e10
Lawrence Murray <lawrence.murray@csiro.au>
2014-10-28 13:53:03 +0800

    Implemented Match class for scoring reference matches.

--- share/src/bi/program/Expression.hpp
+++ share/src/bi/program/Expression.hpp
@@ -8,6 +8,8 @@
 #ifndef BI_PROGRAM_EXPRESSION_HPP
 #define BI_PROGRAM_EXPRESSION_HPP
 
+#include "Match.hpp"
+
 namespace biprog {
 /**
  * Expression.
@@ -16,15 +18,30 @@ namespace biprog {
  */
 class Expression {
 public:
+
   /**
    * Destructor.
    */
   virtual ~Expression() = 0;
+
+  /**
+   * Attempt to match another expression.
+   *
+   * @param o Other expression.
+   * @param[out] match The match, if any.
+   *
+   * @return Does #o match?
+   */
+  virtual bool match(Expression* o, Match& match);
 };
 }
 
 inline biprog::Expression::~Expression() {
   //
+}
+
+inline bool biprog::Expression::match(Expression* o, Match& match) {
+  match.push(o, this, Match::SCORE_EXPRESSION);
 }
 
 #endif
---
+++ share/src/bi/program/Match.hpp
@@ -0,0 +1,117 @@
+/**
+ * @file
+ *
+ * @author Lawrence Murray <lawrence.murray@csiro.au>
+ * $Rev$
+ * $Date$
+ */
+#ifndef BI_PROGRAM_MATCH_HPP
+#define BI_PROGRAM_MATCH_HPP
+
+#include <vector>
+
+namespace biprog {
+class Expression;
+
+/**
+ * Match between expressions.
+ *
+ * @ingroup program
+ */
+class Match {
+public:
+  /**
+   * Scores.
+   */
+  enum Score {
+    /**
+     * Score for match at leaf/concrete level.
+     */
+    SCORE_LEAF,
+
+    /**
+     * Score for match at declaration level.
+     */
+    SCORE_DECLARATION,
+
+    /**
+     * Score for match at statement level.
+     */
+    SCORE_STATEMENT,
+
+    /**
+     * Score for match at expression level.
+     */
+    SCORE_EXPRESSION
+  };
+
+  /**
+   * Comparison operator for sorting matches by score.
+   */
+  bool operator<(const Match& o) const;
+
+  /**
+   * Add a match.
+   *
+   * @param arg The argument.
+   * @param param The formal parameter.
+   * @param score The score.
+   */
+  void push(Expression* arg, Expression* param, const Score score);
+
+  /**
+   * Clear for reuse.
+   */
+  void clear();
+
+private:
+  /**
+   * Arguments.
+   */
+  std::vector<Expression*> args;
+
+  /**
+   * Parameters.
+   */
+  std::vector<Expression*> params;
+
+  /**
+   * Scores.
+   */
+  std::vector<int> scores;
+};
+}
+
+#include "../misc/assert.hpp"
+
+#include "boost/typeof/typeof.hpp"
+
+inline bool biprog::Match::operator<(const Match& o) const {
+  /* pre-condition */
+  BI_ASSERT(scores.size() == o.scores.size());
+
+  BOOST_AUTO(iter1, scores.begin());
+  BOOST_AUTO(iter2, o.scores.begin());
+  bool result = scores.size() > 0;
+  while (result && iter1 != scores.end()) {
+    result = *iter1 < *iter2;
+    ++iter1;
+    ++iter2;
+  }
+  return result;
+}
+
+inline void biprog::Match::push(Expression* arg, Expression* param,
+    const Score score) {
+  args.push_back(arg);
+  params.push_back(param);
+  scores.push_back(score);
+}
+
+inline void biprog::Match::clear() {
+  args.clear();
+  params.clear();
+  scores.clear();
+}
+
+#endif
