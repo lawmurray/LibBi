f97b3c8ef85d5e08cf3d534037b377bbe55ed1a1
Lawrence Murray <murray@stats.ox.ac.uk>
2016-01-27 17:03:19 +0000

    Build updates to make MAGMA headers optional, removed some LibBi v2 files that had crept in during earlier work.

--- share/src/bi/cuda/math/magma.hpp
+++ share/src/bi/cuda/math/magma.hpp
@@ -13,6 +13,15 @@
 #include "cublas.hpp"
 #ifdef HAVE_MAGMA_H
 #include "magma.h"
+#else
+int magma_dpotrf_gpu(char uplo, int n, double *A, int lda, int *info);
+int magma_spotrf_gpu(char uplo, int n, float *A, int lda, int *info);
+
+int magma_dpotrs_gpu(char uplo, int n, int nrhs, double* dA, int ldda, double* dB, int lddb, int *info);
+int magma_spotrs_gpu(char uplo, int n, int nrhs, float* dA, int ldda, float* dB, int lddb, int *info);
+
+int magma_get_dpotrf_nb(int m);
+int magma_get_spotrf_nb(int m);
 #endif
 
 #include "boost/typeof/typeof.hpp"
@@ -38,11 +47,9 @@ namespace bi { \
   }; \
 }
 
-#ifdef HAVE_MAGMA_H
 MAGMA_FUNC(potrf, dpotrf_gpu, spotrf_gpu)
 MAGMA_FUNC(potrs, dpotrs_gpu, spotrs_gpu)
 MAGMA_FUNC(get_potrf_nb, get_dpotrf_nb, get_spotrf_nb)
 //MAGMA_FUNC(syevx, dsyevx_gpu, ssyevx_gpu)
-#endif
 
 #endif
