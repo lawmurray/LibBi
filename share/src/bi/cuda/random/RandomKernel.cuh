lawmurray/LibBi

commit 951c1e20d3372f806fc0345b123b4c46f6487e3e

discrete probability distributions

share/src/bi/cuda/random/RandomKernel.cuh
@@ -118,4 +118,18 @@ CUDA_FUNC_GLOBAL void bi::kernelGammas(curandStateSA rng, V1 x,
   rng.store(q, rng1.r);
 }
 
+template<class V1>
+CUDA_FUNC_GLOBAL void bi::kernelPoissons(curandStateSA rng, V1 x,
+    const typename V1::value_type lambda) {
+  const int q = blockIdx.x*blockDim.x + threadIdx.x;
+  const int Q = blockDim.x*gridDim.x;
+
+  RngGPU rng1;
+  rng.load(q, rng1.r);
+  for (int p = q; p < x.size(); p += Q) {
+    x(p) = rng1.poisson(lambda);
+  }
+  rng.store(q, rng1.r);
+}
+
 #endif

share/src/bi/cuda/random/RngGPU.cuh
@@ -137,4 +137,12 @@ inline T1 bi::RngGPU::gamma(const T1 alpha, const T1 beta) {
   return scale*dv;
 }
 
+inline float bi::RngGPU::poisson(const float lambda) {
+  return curand_poisson(&r, lambda);
+}
+
+inline double bi::RngGPU::poisson(const double lambda) {
+  return curand_poisson(&r);
+}
+
 #endif

MANIFEST
@@ -34,8 +34,10 @@ lib/Bi/Action/inclusive_scan.pm
 lib/Bi/Action/inverse_gamma.pm
 lib/Bi/Action/log_gaussian.pm
 lib/Bi/Action/log_normal.pm
+lib/Bi/Action/negbin.pm
 lib/Bi/Action/normal.pm
 lib/Bi/Action/ode_.pm
+lib/Bi/Action/poisson.pm
 lib/Bi/Action/pdf.pm
 lib/Bi/Action/std_.pm
 lib/Bi/Action/transpose.pm
@@ -631,7 +633,9 @@ share/tt/cpp/action/inclusive_scan.hpp.tt
 share/tt/cpp/action/inverse_gamma.hpp.tt
 share/tt/cpp/action/misc/footer.hpp.tt
 share/tt/cpp/action/misc/header.hpp.tt
+share/tt/cpp/action/negbin.hpp.tt
 share/tt/cpp/action/ode_.hpp.tt
+share/tt/cpp/action/poisson.hpp.tt
 share/tt/cpp/action/pdf.hpp.tt
 share/tt/cpp/action/std_.hpp.tt
 share/tt/cpp/action/transpose.hpp.tt

lib/Bi/Action/negbin.pm
@@ -1,0 +1,90 @@
+=head1 NAME
+
+negbin - Negative binomial distribution.
+
+=head1 SYNOPSIS
+
+    x ~ negbin()
+    x ~ negbin(1.0, 2.0)
+    x ~ poisson(mean = 1.0, shape = 2.0)
+
+=head1 DESCRIPTION
+
+A C<negbin> action specifies that a variable is distributed according to
+a negative binomial distribution the given C<mean> and C<shape> parameters.
+Note that the implementation will evaluate densities for any (not necessarily
+integer) x. It is left to the user to ensure consistency (e.g., using this only
+with integer observations).
+
+
+=cut
+
+package Bi::Action::negbin;
+
+use parent 'Bi::Action';
+use warnings;
+use strict;
+
+=head1 PARAMETERS
+
+=over 4
+
+=item C<mean> (position 0, default 1.0)
+
+Mean parameter of the distribution.
+
+=item C<shape> (position 1, default 1.0)
+
+Shape parameter of the distribution.
+
+=back
+
+=cut
+our $ACTION_ARGS = [
+  {
+    name => 'mean',
+    positional => 1,
+    default => 1.0
+  },
+  {
+    name => 'shape',
+    positional => 1,
+    default => 1.0
+  },
+];
+
+sub validate {
+    my $self = shift;
+    
+    Bi::Action::validate($self);
+    $self->process_args($ACTION_ARGS);
+    $self->ensure_op('~');
+    $self->ensure_scalar('mean');
+    $self->ensure_scalar('shape');
+
+    unless ($self->get_left->get_shape->compat($self->get_shape)) {
+    	die("incompatible sizes on left and right sides of action.\n");
+    }
+
+    $self->set_parent('pdf_');
+    $self->set_can_combine(1);
+    $self->set_unroll_args(0);
+}
+
+sub mean {
+    my $self = shift;
+
+    my $mean = $self->get_named_arg('mean');
+
+    return $mean;
+}
+
+1;
+
+=head1 AUTHOR
+
+Sebastian Funk <sebastian.funk@lshtm.ac.uk>
+
+=head1 VERSION
+
+$Rev$ $Date$

lib/Bi/Action/poisson.pm
@@ -1,0 +1,78 @@
+=head1 NAME
+
+poisson - Poisson distribution.
+
+=head1 SYNOPSIS
+
+    x ~ poisson()
+    x ~ poisson(1.0)
+    x ~ poisson(rate = 2.0)
+
+=head1 DESCRIPTION
+
+A C<poisson> action specifies that a variable is Poisson distributed according to
+the given C<rate> parameter. Note that the implementation will evaluate
+densities for any (not necessarily integer) x. It is left to the user to ensure
+consistency (e.g., using this only with integer observations).
+
+=cut
+
+package Bi::Action::poisson;
+
+use parent 'Bi::Action';
+use warnings;
+use strict;
+
+=head1 PARAMETERS
+
+=over 4
+
+=item C<rate> (position 0, default 1.0)
+
+Rate parameter of the distribution.
+
+=back
+
+=cut
+our $ACTION_ARGS = [
+  {
+    name => 'rate',
+    positional => 1,
+    default => 1.0
+  }
+];
+
+sub validate {
+    my $self = shift;
+    
+    Bi::Action::validate($self);
+    $self->process_args($ACTION_ARGS);
+    $self->ensure_op('~');
+    $self->ensure_scalar('rate');
+
+    unless ($self->get_left->get_shape->compat($self->get_shape)) {
+    	die("incompatible sizes on left and right sides of action.\n");
+    }
+
+    $self->set_parent('pdf_');
+    $self->set_can_combine(1);
+    $self->set_unroll_args(0);
+}
+
+sub mean {
+    my $self = shift;
+
+    my $mean = $self->get_named_arg('rate');
+
+    return $mean;
+}
+
+1;
+
+=head1 AUTHOR
+
+Sebastian Funk <sebastian.funk@lshtm.ac.uk>
+
+=head1 VERSION
+
+$Rev$ $Date$

share/configure.ac
@@ -239,6 +239,7 @@ AC_CHECK_HEADERS([\
     boost/random/gamma_distribution.hpp \
     boost/random/mersenne_twister.hpp \
     boost/random/normal_distribution.hpp \
+    boost/random/poisson_distribution.hpp \
     boost/random/uniform_int.hpp \
     boost/random/uniform_real.hpp \
     boost/random/variate_generator.hpp \

share/src/bi/cuda/random/RandomGPU.cuh
@@ -45,6 +45,17 @@ void bi::RandomGPU::gammas(Random& rng, V1 x,
   CUDA_CHECK;
 }
 
+template<class V1>
+void bi::RandomGPU::poissons(Random& rng, V1 x,
+    const typename V1::value_type lambda) {
+  dim3 Db, Dg;
+  Db.x = bi::min(x.size(), deviceIdealThreadsPerBlock());
+  Dg.x = (bi::min(x.size(), deviceIdealThreads()) + Db.x - 1) / Db.x;
+
+  kernelPoissons<<<Dg,Db>>>(rng.devRngs, x, lambda);
+  CUDA_CHECK;
+}
+
 template<class V1, class V2>
 void bi::RandomGPU::multinomials(Random& rng, const V1 lps, V2 xs) {
   BI_ERROR_MSG(false, "Not implemented on device");

share/src/bi/cuda/random/RandomGPU.hpp
@@ -49,6 +49,13 @@ struct RandomGPU {
       1.0, const typename V1::value_type beta = 1.0);
 
   /**
+   * @copydoc Random::poissons
+   */
+  template<class V1>
+  static void poissons(Random& rng, V1 x, const typename V1::value_type labmda =
+                     1.0);
+
+  /**
    * @copydoc Random::gammas
    */
   template<class V1>

commit 55ece42aa93e1b29c1ae0352f27d80d17f30668b

fix CUDA compiler errors introduced by commit 951c1e20d3372f806fc0345b123b4c46f6487e3e

share/src/bi/cuda/random/RandomKernel.cuh
@@ -70,6 +70,18 @@ CUDA_FUNC_GLOBAL void kernelGammas(curandStateSA rng, V1 x,
     const typename V1::value_type alpha = 1.0,
     const typename V1::value_type beta = 1.0);
 
+/**
+ * Kernel function to fill vector with poisson variates.
+ *
+ * @tparam V1 Vector type.
+ *
+ * @param[in,out] rng Random number generator.
+ * @param[out] x Vector to fill.
+ * @param lambda rate.
+ */
+template<class V1>
+CUDA_FUNC_GLOBAL void bi::kernelPoissons(curandStateSA rng, V1 x,
+    const typename V1::value_type lambda); 
 }
 
 #include "../../random/Random.hpp"

share/src/bi/cuda/random/RngGPU.cuh
@@ -64,6 +64,16 @@ public:
   CUDA_FUNC_DEVICE T1 gamma(const T1 alpha = 1.0, const T1 beta = 1.0);
 
   /**
+   * @copydoc Random::poisson
+   */
+  CUDA_FUNC_DEVICE float poisson(const float lambda = 0.0);
+
+  /**
+   * @copydoc Random::poisson
+   */
+  CUDA_FUNC_DEVICE double poisson(const double lambda = 0.0);
+
+   /**
    * CURAND state.
    */
   curandState r;
@@ -142,7 +152,7 @@ inline float bi::RngGPU::poisson(const float lambda) {
 }
 
 inline double bi::RngGPU::poisson(const double lambda) {
-  return curand_poisson(&r);
+  return curand_poisson(&r, lambda);
 }
 
 #endif

share/src/bi/cuda/random/curandStateSA.hpp
@@ -13,6 +13,7 @@
 
 #ifdef ENABLE_CUDA
 #include "curand_kernel.h"
+#include "curand_poisson.h"
 #endif
 
 namespace bi {